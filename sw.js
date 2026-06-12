3def5a0b95e3aec2106fd1b5bb92604a4c327c13
Nunera101 <cerqueiragnsla@gmail.com>
2026-06-11 21:24:26 -0300

    fix(aluno): barra inferior do aluno em uma linha com 6 colunas

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿const CACHE_NAME = "personal-pro-pwa-v55";
-const DATA_CACHE_NAME = "personal-pro-data-v55";
+﻿const CACHE_NAME = "personal-pro-pwa-v56";
+const DATA_CACHE_NAME = "personal-pro-data-v56";
 const CRITICAL_ASSETS = new Set(["/", "/index.html", "/app.js", "/styles.css", "/manifest.json"]);
 const APP_SHELL = [
   "./",
